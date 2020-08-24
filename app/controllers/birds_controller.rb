3d29acb3a2e743c0a51bdedc195bb678c04479c4
Elijah wines <elijahwines84@yahoo.com>
2020-08-24 14:11:48 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,7 +5,7 @@ class BirdsController < ApplicationController
   end
 
   def show
-    @bird = Bird.find_by(id: params[:id])
+    bird = Bird.find_by(id: params[:id])
     render json: bird
   end
 end
